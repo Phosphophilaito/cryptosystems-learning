da388f6b39bb80732a699c6ddeaf099d429ac5d1
Jonathan Calderón Mora <phosphophilaito@gmail.com>
2024-08-07 16:18:06 +0200

    Solved bug dechiphering the original hex

--- Set_1_Basics/03_single_byte_xor_cypher/single_byte_cypher.go
+++ Set_1_Basics/03_single_byte_xor_cypher/single_byte_cypher.go
@@ -1,35 +1,79 @@
 package main
 
 import (
+	"encoding/hex"
 	"fmt"
-	"os"
 	"regexp"
 )
 
-func CountLettersFile(name string) {
-	content, err := os.ReadFile(name)
-	if err != nil {
-		fmt.Println("Error reading file:", err)
-		return
-	}
+const hexEncoded = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
 
-	counter := make(map[byte]int)
-	reg, _ := regexp.Compile("a-z")
+func Reverse(s string) string {
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r)
+}
+
+func CountLetters(content []byte) (map[byte]int, int, error) {
+	reg, _ := regexp.Compile("[a-z]")
+	mapCounterLetter := make(map[byte]int)
+	totalLetter := 0
 
 	for _, r := range content {
-		_, found := counter[r]
+
+		_, found := mapCounterLetter[r]
 		match := reg.MatchString(string(r))
+
 		if found && match {
-			counter[r] += 1
+			mapCounterLetter[r]++
+			totalLetter++
 		} else if !found && match {
-			counter[r] = 1
+			mapCounterLetter[r] = 1
+			totalLetter++
 		}
 	}
 
-	fmt.Println(counter)
+	return mapCounterLetter, totalLetter, nil
+}
 
+func GetFrequencyMapLetterCounter(mapCounterLetter map[byte]int, totalLetter int) map[byte]float64 {
+	mapFreqLetter := make(map[byte]float64)
+	for key, value := range mapCounterLetter {
+		mapFreqLetter[key] = float64(value) / float64(totalLetter)
+	}
+	return mapFreqLetter
 }
 
 func main() {
-	CountLettersFile("frankenstein.txt")
+	/*content, err := os.ReadFile("frankenstein.txt")
+	if err != nil {
+		log.Fatal(err)
+		fmt.Println("Error reading file:", err)
+		return
+	}
+	mapCounterLetterText, totalLetterText, err := CountLetters(content)
+
+	mapFreqLetterText := GetFrequencyMapLetterCounter(mapCounterLetterText, totalLetterText)
+	*/
+	hexOriginal, _ := hex.DecodeString(hexEncoded)
+
+	fmt.Println(hexOriginal)
+
+	mapDecypherResp := make(map[byte][]byte)
+
+	for cypherKey := range 256 {
+
+		var decypherHex []byte
+		for _, hexOriginalValue := range hexOriginal {
+			decypherHex = append(decypherHex, hexOriginalValue^byte(cypherKey))
+		}
+
+		mapDecypherResp[byte(cypherKey)] = decypherHex
+	}
+
+	for key, value := range mapDecypherResp {
+		fmt.Println("Key: ", key, " is translated as: ", string(value))
+	}
 }
